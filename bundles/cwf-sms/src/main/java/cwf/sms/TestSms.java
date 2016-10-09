98aaf0174d12f65c9447f9a9b7e68849f9f7cac0
Sandeep Valapi <sandeepvalapi@gmail.com>
2016-10-09 11:49:06 +0530

    Commit From Mac Book

--- bundles/cwf-sms/src/main/java/cwf/sms/TestSms.java
+++ bundles/cwf-sms/src/main/java/cwf/sms/TestSms.java
@@ -1,20 +1,7 @@
 package cwf.sms;
 
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.OutputStreamWriter;
-import java.io.Writer;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 import cwf.sms.expose.SmsUtilities;
 import cwf.sms.expose.SmsUtilitiesImpl;
-import freemarker.template.Configuration;
-import freemarker.template.Template;
-import freemarker.template.TemplateException;
 
 public class TestSms {
 
@@ -27,12 +14,13 @@ public class TestSms {
 
 		SmsUtilities utils = new SmsUtilitiesImpl();
 		// sending message(mobile numbers,message)
-		String messageId = utils.sendSms("9030782250", "Greetings from WhiteBay. Our SMS Channel is working.");
-		System.out.println(messageId);
+		String messageId = utils.sendSms("9959377576",
+				"Greetings from WhiteBay. Our SMS Channel is working.");
+		System.out.println("Message ID : " + messageId);
 		String status = utils.deliveryStatus(messageId);
-		System.out.println(status);
+		System.out.println("Message Status : " + status);
 		String balance = utils.balanceCheck();
-		System.out.println(balance);
+		System.out.println("Balance Count :" + balance);
 
 	}
 }
